6a435b14d256877a9d733d89d8801f07986094ca
makpalu <mawunyolitsky@gmail.com>
2025-06-20 13:28:11 +0000

    Updated code

--- main.py
+++ main.py
@@ -9,40 +9,75 @@ from todolistBSC import Todo, Todolist
 # print(todolist1.get_todo_names())
 
 #Write a program that allows you to create multiple todolist and multiple todos and be able to store the todos and the todolist
+def create_todolists():
+    todolist = Todolist(input("Enter the name of the todolist: "), input("Enter the description: "))
+    todolists.append(todolist)
+
+def create_todo(selected_todolist):
+    todo_name = input("Enter your task: ")
+    todo_id = input("Enter the id: ")
+    todo_level = input("Enter the priority level: ")
+    todo_deadline = input("Enter the deadline: ")
+    todo = Todo(todo_name, todo_id, todo_level, todo_deadline, False, True)
+    selected_todolist.add_todo(todo)
+
+def select_todolist():
+    print("Select the todolist to add it to: ")
+    for index, element in enumerate(todolists, 1):
+        print(f"{index}. {element.get_name()}")
+
+    user_input = 0
+
+    while user_input < 1 or user_input > len(todolists):
+        user_input = int(input(("Enter your option: ")))
+    return todolists[user_input - 1]
+
+
+def view_todos():
+    print("Select the todolist to view from: ")
+    for index, element in enumerate(todolists, 1):
+        print(f"{index}. {element.get_name()}")
+
+    user_input = 0
+
+    while user_input < 1 or user_input > len(todolists):
+        user_input = int(input(("Enter your option: ")))
+    view_selected_todos = todolists[user_input - 1]
+
+    for index, a_todo in enumerate(view_selected_todos.get_todos(), 1):
+        print(f"{index}. {a_todo.get_name()}")
+
+
+
 
 is_running = True
 todolists = []
+selected_todolist = None
+view_selected_todos = None
 
 while is_running:
-    print("Enter 1 to create a todolist")
-    print("Enter 2 to create a todo")
-    print("Enter 3 to quit")
+    print("Enter 1 to create a todolist\nEnter 2 to create a todo\nEnter 3 to view todos\nEnter 4 to quit")
     x = input("x: ")
     if x == "1":
-        todolist = Todolist(input("Enter the name of the todolist: "), input("Enter the description: "))
-        todolists.append(todolist)
+        create_todolists()
+
     elif x == "2":
         if not todolists:
             print("Todolist not available. Create one first.")
             continue
-        print("Select the todolist to add it to: ")
-        for index, element in enumerate(todolists, 1):
-            print(f"{index}. {element.get_name()}")
-
-        user_input = 0
-
-        while user_input < 1 or user_input > len(todolists):
-            user_input = int(input(("Enter your option: ")))
-        selected_todolist = todolists[user_input - 1]
         
-        todo_name = input("Enter your task: ")
-        todo_id = input("Enter the id: ")
-        todo_level = input("Enter the priority level: ")
-        todo_deadline = input("Enter the deadline: ")
-        todo = Todo(todo_name, todo_id, todo_level, todo_deadline, False, True)
-        selected_todolist.add_todo(todo)
+        selected_todolist = select_todolist()
+
+        create_todo(selected_todolist)
+        
     elif x == "3":
+        view_todos()
+    
+    elif x == "4":
         is_running = False
+    
     else:
         print("Enter a valid input.")
-        
+
+
+
